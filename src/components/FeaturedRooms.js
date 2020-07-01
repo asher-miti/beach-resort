63813b2c69fc1215b39af032294e31673456a932
Asher <asher_3@hotmail.co.uk>
2020-07-01 09:54:03 +0100

    working on further context for type of rooms, including price, bed size etc.

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,12 +4,9 @@ import { RoomContext } from "../context";
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { name, greeting } = this.context;
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
 
-    return (
-      <div>
-        {greeting} {name} from featured rooms
-      </div>
-    );
+    return <div>from featured rooms</div>;
   }
 }
