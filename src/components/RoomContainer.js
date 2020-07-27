807ebe0657e96ff6ba3e39a563a16a9633a194e9
Asher <asher_3@hotmail.co.uk>
2020-07-27 20:35:11 +0100

    placing RoomContainer into a higher order component

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,23 +1,41 @@
 import React from "react";
 import RoomFilter from "./RoomFilter";
 import RoomList from "./RoomList";
-import { RoomConsumer } from "../context";
+import { withRoomConsumer, RoomConsumer } from "../context";
 import Loading from "./Loading";
 
-export default function RoomContainer() {
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
   return (
-    <RoomConsumer>
-      {(value) => {
-        console.log(value);
-
-        return (
-          <div>
-            Hello From Rooms Container
-            <RoomFilter />
-            <RoomList />
-          </div>
-        );
-      }}
-    </RoomConsumer>
+    <div>
+      Hello From Rooms Container
+      <RoomFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </div>
   );
 }
+
+export default withRoomConsumer(RoomContainer);
+
+// export default function RoomContainer() {
+//   return (
+//     <RoomConsumer>
+//       {(value) => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello From Rooms Container
+//             <RoomFilter rooms={rooms} />
+//             <RoomList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
